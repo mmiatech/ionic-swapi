8b920e8de68f9b69f491a9a425ca0388f308c733
mmiatech <mmiatech@madisoncollege.edu>
2020-11-19 11:19:39 -0600

    got planet names in and sorted

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -14,8 +14,9 @@ export class SwapiService {
     fetchPlanets() {
       return this.httpSvc.get("https://swapi.dev/api/planets/").pipe(
         tap(x => console.log(x))
-        , repeat(3)
-        , tap(x => console.info(x))
+        , map(x => (x as any).results.map(y => ({name: y.name})))
+        , tap(x => console.log(x))
+        , repeat(10)
       )
     }
    
